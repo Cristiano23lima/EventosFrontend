cd717ab1b46cb7bd8eb51fa49ca99bc21efbedf9
Cristiano Rodrigues <dj7cristiano@gmail.com>
2019-12-13 09:49:07 -0300

    Deploy app angular no heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { HomeComponent } from './components/home/home.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { ViewEventosComponent } from './components/view-eventos/view-eventos.component';
 
-export var options: Partial<IConfig> | (() => Partial<IConfig>);
+export let options: Partial<IConfig> | (() => Partial<IConfig>);
 @NgModule({
   declarations: [
     AppComponent,
