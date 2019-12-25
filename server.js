ea2b2bd27160c5949289d290c35b15a0a2830cd9
Cristiano Rodrigues <dj7cristiano@gmail.com>
2019-12-25 17:39:55 -0300

    Removendo a tag meta

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 
 
-app.use(express.static(`$/dist/$`));
+app.use(express.static(__dirname + '/src/'));
 
 app.get('/*', (req, res) => {//disponibilizando todos os arquivos estáticos
-  res.sendFile(path.join(`$/dist/$/index.html`));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
 app.listen(process.env.PORT || 8080);
